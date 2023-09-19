a15949cca41e0f7daeade48b37cba5c91638c85f
Jared Dietz <jdietz@fnni.com>
2023-09-19 09:04:35 -0500

    add a counter

--- src/main/java/com/example/listener/subscriber/QueueListener.java
+++ src/main/java/com/example/listener/subscriber/QueueListener.java
@@ -12,9 +12,13 @@ import java.util.logging.Logger;
 public class QueueListener {
     private static final Logger logger = Logger.getLogger(QueueListener.class.getName());
 
+    Integer counter = 0;
+
     // {"key": "value", "hello": "world"}
     @SqsListener("notification-queue")
     public void receiveMessage(Message<String> message) {
+        counter++;
+        logger.info(String.format("Messages receieved: %d", counter));
         logger.info("Headers");
         message.getHeaders().forEach((key, value) -> logger.info(String.format("%s: %s", key, value)));
         logger.info(String.format("Received: %s", message.getPayload()));
